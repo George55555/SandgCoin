0ed5f767ea4136d9a710c392cc86724392359af6
George55555 <ggborushko@yahoo.com>
2020-05-11 22:34:43 -0400

    Added CLIENT_VERSION_SUFFIX

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("SandGCoin");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "SandGCoin"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
